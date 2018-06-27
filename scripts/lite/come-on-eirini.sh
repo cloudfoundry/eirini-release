e0afa22465a32d5f86947fbbc8db4306ffded276
Georgi Dankov <gddankov@gmail.com>
2018-06-27 15:46:19 +0200

    fix eirini deploy scripts
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- scripts/lite/come-on-eirini.sh
+++ scripts/lite/come-on-eirini.sh
@@ -17,13 +17,12 @@ clone_repos() {
   pushd $EIRINI_LITE
 	  git clone https://github.com/cloudfoundry/bosh-deployment.git
 	  git clone https://github.com/cloudfoundry/cf-deployment.git
-		#git clone -b develop --single-branch https://github.com/JulzDiverse/capi-release.git
-	  git clone https://github.com/cloudfoundry/capi-release.git
+		git clone -b develop --single-branch https://github.com/JulzDiverse/capi-release.git
 	popd
 }
 
 set_env() {
-  export BOSH_DEPLOYMENT=${BOSH_DEPLOYMENT:-"$EIRINI_LITE/bosh-deployment"}
+  export BOSH_DEPLOYMENT_DIR=${BOSH_DEPLOYMENT_DIR:-"$EIRINI_LITE/bosh-deployment"}
 	export CF_DEPLOYMENT=${CF_DEPLOYMENT:-"$EIRINI_LITE/cf-deployment"}
 	export EIRINI_RELEASE=${EIRINI_RELEASE:-"$BASEDIR"}
 	export CAPI_RELEASE=${CAPI_RELEASE:-"$EIRINI_LITE/capi-release"}
