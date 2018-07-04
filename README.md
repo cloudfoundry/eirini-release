404b2e35e8743a0831b1650912a5a53986a311df
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-07-04 11:38:26 +0200

    update README
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 This is a BOSH release for [eirini](https://code.cloudfoundry.org/eirini).
 
+- This is an **experimental** release and is still considered _work in progress_.
+- In all examples, we refer to `bosh` as an alias to `bosh2` CLI.
+
 ## CI Pipeline
 
 - [Eirini-CI](https://flintstone.ci.cf-app.com/teams/eirini/pipelines/eirini-ci)
-
-## Description
-_Note_: This is an **Experimental** release and is still considered _work in progress_.<br />
-_Note_: In all examples, we refer to `bosh` as an alias to `bosh2` CLI.<br />
 
 ## Deploy Eirini-Release
 
@@ -17,26 +16,34 @@ _Note_: In all examples, we refer to `bosh` as an alias to `bosh2` CLI.<br />
 Make sure to have the following tools deployed to your local machine:
 
 - [Virtual Box](https://www.virtualbox.org/)
-- [Docker](https://docs.docker.com/install/) 
+- [Docker](https://docs.docker.com/install/)
 - [Bosh (v2) CLI](https://bosh.io/docs/cli-v2-install/)
 - [CF CLI](https://docs.cloudfoundry.org/cf-cli/install-go-cli.html)
 - [minikube](https://github.com/kubernetes/minikube#installation)
 - [Kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
 - Ruby ( v > 2.4:`capi-release` still uses pre-packaging)
+- Golang
 
-### Deploying 
+### Deploying
 
-Download the `eirini-release` and execute `$eirini-release/scripts/lite/deploy-eirini-lite.sh` script:
+The deploy script will fetch all necessary resources into a folder that is determined by the environment variable `$EIRINI_LITE`. When not set, the default will be `~/workspace/eirini-lite`.
 
-```
-$ source ~/workspace/eirini-release/scripts/lite/come-on-eirini.sh
-```
+1. Get `eirini-release`
 
-This script will create a `eirini-lite` folder in your `$HOME/workspace` directory, download all necessary repositories and setup bosh-lite, minikube, plus eirini on your local machine. Moreover it will setup a `eirini` org and `dev` space on CF, such that you are ready to push some `dora` apps right after the script finished its work (which takes a while). 
+	 Clone `eirini-release` to your local machine into `$EIRINI_LITE`.
+	 
+1. Execute the deploy script:
 
-If you want to do things manually or have a running Bosh-Lite and CF, you can take a look at our script `scripts/lite/setup-eirini-environment.sh`. It is well written and documented. Each required step is a separate function (they contain the commands that needs to be executed for the particular step) and the `main` tells you the order of the required steps.
+   ```
+   $ source $EIRINI_LITE/eirini-release/scripts/lite/come-on-eirini.sh
+   ```
+
+   This script will setup bosh-lite, minikube, plus eirini on your local machine. Moreover it will setup an `eirini` org and `dev` space on CF, such that you are ready to push some apps right after the script finished its work (which takes a while). 
+
+   If you want to do things manually or have a running Bosh-Lite and CF, you can take a look at our script `scripts/lite/setup-eirini-environment.sh`. It should explain the steps necessary thoroughly. 
 
 ## Contributing
+
 1. Fork this project into your GitHub organisation or username
 1. Make sure you are up-to-date with the upstream master and then create your feature branch (`git checkout -b amazing-new-feature`)
 1. Add and commit the changes (`git commit -am 'Add some amazing new feature'`)
--- scripts/lite/setup-eirini-environment.sh
+++ scripts/lite/setup-eirini-environment.sh
@@ -1,134 +1,158 @@
 #!/bin/bash
 
-main() {
-	check_env
+# Each required step is a separate function (they contain the commands that needs to be executed for the particular step) and the `main` tells you the order of the required steps.
 
-	echo ":::::::Setting up Bosh-Director"
+# shellcheck disable=SC2164
+
+main() {
+  echo "::::::: Checking environment"
+  check_env
+
+  echo "::::::: Setting up Bosh-Director"
   deploy_director
 
-	echo ":::::::Add Eirini routes to Director VM"
-	add_eirini_routes
+  echo "::::::: Add Eirini routes to Director VM"
+  add_eirini_routes
 
-  echo ":::::::Setting up minikube"
-	setup_minikube
+  echo "::::::: Setting up minikube"
+  setup_minikube
 
-	echo ":::::::Preaparing eirini release"
+  echo "::::::: Preaparing eirini release"
   prepare_eirini_release
 
-	echo ":::::::Preaparing capi release"
-	prepare_capi_release
+  echo "::::::: Preaparing capi release"
+  prepare_capi_release
 
-	echo ":::::::Deploying CF and Eirini"
-	deploy_cf_and_eirini
+  echo "::::::: Deploying CF and Eirini"
+  deploy_cf_and_eirini
 
-	echo ":::::::Setting routes to reach CF API"
-	enable_cf_api
+  echo "::::::: Setting routes to reach CF API"
+  enable_cf_api
 
-	echo ":::::::Setup CF environment"
-	setup_cf_environment
+  echo "::::::: Setup CF environment"
+  setup_cf_environment
 
-	echo ":::::::Summary"
-	summary
+  echo "::::::: Summary"
+  summary
 }
 
 check_env() {
   echo "Using the following environment:"
-	echo "BOSH_DEPLOYMENT: ${BOSH_DEPLOYMENT_DIR?"Environment variable BOSH_DEPLOYMENT_DIR not set"}"
-	echo "CF_DEPLOYMENT: ${CF_DEPLOYMENT?"Environment variable CF_DEPLOYMENT not set"}"
-	echo "EIRINI_RELEASE: ${EIRINI_RELEASE?"Environment variable EIRINI_RELEASE not set"}"
-	echo "CAPI_RELEASE: ${CAPI_RELEASE?"Environment variable CAPI_RELEASE not set"}"
-	echo "BOSH_DIRECTOR_ALIAS: ${BOSH_DIRECTOR_ALIAS?"Environment variable BOSH_DIRECTOR_ALIAS not set"}"
-	echo "EIRINI_LITE: ${EIRINI_LITE?"Environment variable EIRINI_LITE (workspace) is not set"}"
+  echo "BOSH_DEPLOYMENT: ${BOSH_DEPLOYMENT_DIR?"Environment variable BOSH_DEPLOYMENT_DIR not set"}"
+  echo "CF_DEPLOYMENT: ${CF_DEPLOYMENT?"Environment variable CF_DEPLOYMENT not set"}"
+  echo "EIRINI_RELEASE: ${EIRINI_RELEASE?"Environment variable EIRINI_RELEASE not set"}"
+  echo "CAPI_RELEASE: ${CAPI_RELEASE?"Environment variable CAPI_RELEASE not set"}"
+  echo "BOSH_DIRECTOR_ALIAS: ${BOSH_DIRECTOR_ALIAS?"Environment variable BOSH_DIRECTOR_ALIAS not set"}"
+  echo "EIRINI_LITE: ${EIRINI_LITE?"Environment variable EIRINI_LITE (workspace) is not set"}"
 }
 
 deploy_director() {
   bosh create-env "$BOSH_DEPLOYMENT_DIR"/bosh.yml \
     --state "$BOSH_DEPLOYMENT_DIR"/state.json \
-    -o "$BOSH_DEPLOYMENT_DIR"/virtualbox/cpi.yml \
-    -o "$BOSH_DEPLOYMENT_DIR"/virtualbox/outbound-network.yml \
-    -o "$BOSH_DEPLOYMENT_DIR"/bosh-lite.yml \
-    -o "$BOSH_DEPLOYMENT_DIR"/bosh-lite-runc.yml \
-    -o "$BOSH_DEPLOYMENT_DIR"/jumpbox-user.yml \
+    --ops-file "$BOSH_DEPLOYMENT_DIR"/virtualbox/cpi.yml \
+    --ops-file "$BOSH_DEPLOYMENT_DIR"/virtualbox/outbound-network.yml \
+    --ops-file "$BOSH_DEPLOYMENT_DIR"/bosh-lite.yml \
+    --ops-file "$BOSH_DEPLOYMENT_DIR"/bosh-lite-runc.yml \
+    --ops-file "$BOSH_DEPLOYMENT_DIR"/jumpbox-user.yml \
     --vars-store "$BOSH_DEPLOYMENT_DIR"/creds.yml \
-    -v director_name="Bosh Lite Director" \
-    -v internal_ip=192.168.50.6 \
-    -v internal_gw=192.168.50.1 \
-    -v internal_cidr=192.168.50.0/24 \
-    -v outbound_network_name=NatNetwork
+    --var director_name="Bosh Lite Director" \
+    --var internal_ip=192.168.50.6 \
+    --var internal_gw=192.168.50.1 \
+    --var internal_cidr=192.168.50.0/24 \
+    --var outbound_network_name=NatNetwork
 
-  bosh -e 192.168.50.6 --ca-cert <(bosh int "$BOSH_DEPLOYMENT_DIR"/creds.yml --path /director_ssl/ca) alias-env "$BOSH_DIRECTOR_ALIAS"
+  bosh --environment 192.168.50.6 \
+    --ca-cert <(bosh int "$BOSH_DEPLOYMENT_DIR"/creds.yml \
+    --path /director_ssl/ca) \
+    alias-env "$BOSH_DIRECTOR_ALIAS"
 
   export BOSH_CLIENT=admin
   bosh_client_secret=$(bosh int "$BOSH_DEPLOYMENT_DIR"/creds.yml --path /admin_password)
   export BOSH_CLIENT_SECRET=$bosh_client_secret
 
-  bosh -e "$BOSH_DIRECTOR_ALIAS" env
+  bosh --environment "$BOSH_DIRECTOR_ALIAS" env
 }
 
 # In order to make Eirini accessible from Kubernetes, we need two iptable rules on the director
 # to redirect traffic to eirini opi (port 8085) and registry (port 8080)
 add_eirini_routes(){
-	bosh int "$BOSH_DEPLOYMENT_DIR"/creds.yml --path /jumpbox_ssh/private_key > "$BOSH_DEPLOYMENT_DIR"/jumpbox.key && chmod 600 "$BOSH_DEPLOYMENT_DIR"/jumpbox.key
-  ssh -o "StrictHostKeyChecking no" jumpbox@192.168.50.6 -i "$BOSH_DEPLOYMENT_DIR"/jumpbox.key 'sudo iptables -t nat -I PREROUTING 2 -p tcp --dport 8090 -j DNAT --to 10.244.0.142:8085'
-  ssh -o "StrictHostKeyChecking no" jumpbox@192.168.50.6 -i "$BOSH_DEPLOYMENT_DIR"/jumpbox.key 'sudo iptables -t nat -I PREROUTING 2 -p tcp --dport 8089 -j DNAT --to 10.244.0.142:8080'
+  bosh interpolate "$BOSH_DEPLOYMENT_DIR"/creds.yml \
+    --path /jumpbox_ssh/private_key > "$BOSH_DEPLOYMENT_DIR"/jumpbox.key \
+    && chmod 600 "$BOSH_DEPLOYMENT_DIR"/jumpbox.key
+
+  ssh -o "StrictHostKeyChecking no" jumpbox@192.168.50.6 \
+    -i "$BOSH_DEPLOYMENT_DIR"/jumpbox.key \
+    'sudo iptables -t nat -I PREROUTING 2 -p tcp --dport 8090 -j DNAT --to 10.244.0.142:8085'
+
+  ssh -o "StrictHostKeyChecking no" jumpbox@192.168.50.6 \
+    -i "$BOSH_DEPLOYMENT_DIR"/jumpbox.key \
+    'sudo iptables -t nat -I PREROUTING 2 -p tcp --dport 8089 -j DNAT --to 10.244.0.142:8080'
 }
 
-# Minikube needs to be started in the same network as bosh-lite, s.t it is able to communicate to CF.
-# Moreover it requires Eirini to be set as insecure-registry, otherwise Kubernetes will reject traffic from/to the registry
+# Minikube needs to be started in the same network as bosh-lite, such that it is able to communicate with CF.
+# Moreover it requires Eirini to be set as insecure-registry, otherwise Kubernetes will reject traffic from/to the registry.
 setup_minikube() {
-  minikube start --host-only-cidr 192.168.50.1/24 --insecure-registry="10.244.0.142:8080"
+  minikube start \
+    --host-only-cidr 192.168.50.1/24 \
+    --insecure-registry="10.244.0.142:8080"
 }
 
 prepare_eirini_release() {
-	pushd "$EIRINI_RELEASE"
-	git submodule update --init --recursive
-	export GOPATH="$EIRINI_RELEASE"
-  ./scripts/buildfs.sh
-	bosh sync-blobs
-	popd
+  pushd "$EIRINI_RELEASE"
+    git submodule update --init --recursive
+    export GOPATH="$EIRINI_RELEASE"
+    ./scripts/buildfs.sh
+    bosh sync-blobs
+  popd
 }
 
+
 prepare_capi_release() {
-	pushd "$CAPI_RELEASE"
-	git submodule update --init --recursive
-	bosh sync-blobs
-	popd
+  pushd "$CAPI_RELEASE"
+    git submodule update --init --recursive
+    bosh sync-blobs
+  popd
 }
 
 
 deploy_cf_and_eirini() {
-	bosh -e "$BOSH_DIRECTOR_ALIAS"  update-cloud-config -n $CF_DEPLOYMENT/iaas-support/bosh-lite/cloud-config.yml
+  bosh -e "$BOSH_DIRECTOR_ALIAS"  update-cloud-config -n "$CF_DEPLOYMENT/iaas-support/bosh-lite/cloud-config.yml"
 
   bosh int "$CF_DEPLOYMENT"/cf-deployment.yml \
-     -o "$CF_DEPLOYMENT"/operations/experimental/enable-bpm.yml \
-     -o "$CF_DEPLOYMENT"/operations/experimental/use-bosh-dns.yml \
-     -o "$CF_DEPLOYMENT"/operations/bosh-lite.yml \
-     -o "$EIRINI_RELEASE"/operations/eirini-bosh-operations.yml \
-     -o "$EIRINI_RELEASE"/operations/dev-version.yml \
-		 -o "$EIRINI_RELEASE"/operations/capi-dev-version.yml \
+     --ops-file "$CF_DEPLOYMENT"/operations/experimental/enable-bpm.yml \
+     --ops-file "$CF_DEPLOYMENT"/operations/experimental/use-bosh-dns.yml \
+     --ops-file "$CF_DEPLOYMENT"/operations/bosh-lite.yml \
+     --ops-file "$EIRINI_RELEASE"/operations/eirini-bosh-operations.yml \
+     --ops-file "$EIRINI_RELEASE"/operations/dev-version.yml \
+     --ops-file "$EIRINI_RELEASE"/operations/capi-dev-version.yml \
      --vars-store "$CF_DEPLOYMENT"/deployment-vars.yml \
      --var=k8s_flatten_cluster_config="$(kubectl config view --flatten=true)" \
-     -v system_domain=bosh-lite.com \
-     -v cc_api=https://api.bosh-lite.com \
-     -v kube_namespace="default" \
-     -v kube_endpoint="no-endpoint" \
-     -v nats_ip=10.244.0.129 \
-     -v registry_address="10.244.0.142:8080" \
-     -v eirini_ip=10.244.0.142 \
-     -v eirini_address="http://eirini.bosh-lite.com:8090" \
-     -v eirini_local_path="$EIRINI_RELEASE" \
-		 -v capi_local_path="$CAPI_RELEASE" > "$EIRINI_LITE"/manifest.yml
+     --var system_domain=bosh-lite.com \
+     --var cc_api=https://api.bosh-lite.com \
+     --var kube_namespace="default" \
+     --var kube_endpoint="no-endpoint" \
+     --var nats_ip=10.244.0.129 \
+     --var registry_address="10.244.0.142:8080" \
+     --var eirini_ip=10.244.0.142 \
+     --var eirini_address="http://eirini.bosh-lite.com:8090" \
+     --var eirini_local_path="$EIRINI_RELEASE" \
+     --var capi_local_path="$CAPI_RELEASE" > "$EIRINI_LITE"/manifest.yml
 
-	STEMCELL_VERSION=$(bosh int "$EIRINI_LITE"/manifest.yml --path /stemcells/alias=default/version)
+  STEMCELL_VERSION=$(bosh int "$EIRINI_LITE"/manifest.yml --path /stemcells/alias=default/version)
 
-	bosh -e "$BOSH_DIRECTOR_ALIAS" upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=$STEMCELL_VERSION
+  bosh --environment "$BOSH_DIRECTOR_ALIAS" \
+    upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$STEMCELL_VERSION"
 
-	bosh -e "$BOSH_DIRECTOR_ALIAS" deploy -d cf -n "$EIRINI_LITE"/manifest.yml
+  bosh --environment "$BOSH_DIRECTOR_ALIAS" deploy \
+    --deployment cf \
+    --non-interactive \
+    "$EIRINI_LITE"/manifest.yml
+
   verify_exit_code $? "Failed to deploy CF"
 }
 
 enable_cf_api(){
-	echo "Need root password for setting routes:"
+  echo "Need root password for setting routes:"
   if [ "$(uname)" = "Darwin" ]; then
     sudo route add -net 10.244.0.0/16 192.168.50.6
   elif [ "$(uname)" = "Linux" ]; then
@@ -144,39 +168,39 @@ enable_cf_api(){
 }
 
 setup_cf_environment(){
-	cf api api.bosh-lite.com --skip-ssl-validation
-	verify_exit_code $? "failed to set api endpoint"
+  cf api api.bosh-lite.com --skip-ssl-validation
+  verify_exit_code $? "failed to set api endpoint"
 
-	cf_admin_pass=$(bosh int "$CF_DEPLOYMENT"/deployment-vars.yml --path /cf_admin_password)
-	cf auth admin "$cf_admin_pass"
-	verify_exit_code $? "failed to authenticate to CF"
+  cf_admin_pass=$(bosh int "$CF_DEPLOYMENT"/deployment-vars.yml --path /cf_admin_password)
+  cf auth admin "$cf_admin_pass"
+  verify_exit_code $? "failed to authenticate to CF"
 
   cf create-org eirini
-	verify_exit_code $? "failed to authenticate to CF"
+  verify_exit_code $? "failed to authenticate to CF"
 
-	cf target -o eirini
-	verify_exit_code $? "failed to authenticate to CF"
+  cf target -o eirini
+  verify_exit_code $? "failed to authenticate to CF"
 
-	cf create-space dev
-	verify_exit_code $? "failed to authenticate to CF"
+  cf create-space dev
+  verify_exit_code $? "failed to authenticate to CF"
 
-	cf target -s dev
-	verify_exit_code $? "failed to authenticate to CF"
+  cf target -s dev
+  verify_exit_code $? "failed to authenticate to CF"
 }
 
 verify_exit_code() {
-	local exit_code=$1
-	local error_msg=$2
-	if [ "$exit_code" -ne 0 ]; then
-		echo "$error_msg"
-		exit 1
-	fi
+  local exit_code=$1
+  local error_msg=$2
+  if [ "$exit_code" -ne 0 ]; then
+    echo "$error_msg"
+    exit 1
+  fi
 }
 
 summary(){
-	cat << EOF
+  cat << EOF
 
-      Welcome To:
+      Come On
       _/_/_/_/  _/_/_/  _/_/_/    _/_/_/  _/      _/  _/_/_/
      _/          _/    _/    _/    _/    _/_/    _/    _/
     _/_/_/      _/    _/_/_/      _/    _/  _/  _/    _/
