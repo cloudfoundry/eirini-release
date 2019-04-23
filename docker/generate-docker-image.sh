67dd94c0efdece59eba9f100068b4135d1d61838
Akshay Mankar <amankar@pivotal.io>
2019-04-23 12:00:29 +0100

    Remove unnecessary exit code check
    `set -e` FTW!
    
    [#162699664]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- docker/generate-docker-image.sh
+++ docker/generate-docker-image.sh
@@ -16,12 +16,10 @@ main(){
 
 build_opi(){
   GOPATH="$BASEDIR" GOOS=linux CGO_ENABLED=0 go build -a -o "$DOCKERDIR/opi/opi" code.cloudfoundry.org/eirini/cmd/opi
-  verify_exit_code $? "Failed to build eirini"
 }
 
 build_rootfs_patcher() {
   GOPATH="$BASEDIR" GOOS=linux CGO_ENABLED=0 go build -a -o "$DOCKERDIR/rootfs-patcher/rootfs-patcher" code.cloudfoundry.org/eirini/cmd/rootfs-patcher
-  verify_exit_code $? "Failed to build rootfs-patcher"
 }
 
 create_docker_images() {
@@ -37,19 +35,12 @@ create_image() {
 
   echo "Creating $image_name docker image..."
   pushd "$path" || exit
-    docker build . -t "${image_name}:$TAG"
-    verify_exit_code $? "Failed to create $image_name docker image"
+    if ! docker build . -t "${image_name}:$TAG"; then
+      echo "Failed to create $image_name docker image"
+      exit 1
+    fi
   popd || exit
   echo "$image_name docker image created!"
-}
-
-verify_exit_code() {
-    local exit_code=$1
-    local error_msg=$2
-    if [ "$exit_code" -ne 0 ]; then
-        echo "$error_msg"
-        exit 1
-    fi
 }
 
 main
