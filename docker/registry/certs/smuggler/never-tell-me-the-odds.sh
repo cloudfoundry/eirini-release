33aae9b9272f8404859fd97deae9e52b9dd06c3a
Mario Nitchev <marionitchev@gmail.com>
2019-07-11 17:46:50 +0300

    Create registry-credentials secret in opi namespace
    * The secret is used to authenticate against the bits-service registry
    
    [#166795492]
    
    Signed-off-by: Michael Schaefer <schaefer.michael@de.ibm.com>

--- docker/registry/certs/smuggler/never-tell-me-the-odds.sh
+++ docker/registry/certs/smuggler/never-tell-me-the-odds.sh
@@ -3,9 +3,26 @@
 set -xeuo pipefail
 IFS=$'\n\t'
 
-scf_secrets="$(kubectl get secret "$SECRET_NAME" --namespace="$SCF_NAMESPACE" --export -o yaml | grep -E 'cc-.*|internal-ca-.*')"
+main(){
+  create-registry-secret
+  create-image-pull-secret
+}
 
-cat <<EOT >> secret.yml
+create-image-pull-secret(){
+  local username password
+  username="$(grep -E "signing_users:.*" -A 2 /config/bits-config |  awk -F: '/username:/{print $2}' | awk '{$1=$1};1')"
+  password="$(grep -E "signing_users:.*" -A 2 /config/bits-config |  awk -F: '/password:/{print $2}' | awk '{$1=$1};1')"
+  kubectl create secret docker-registry "$REGISTRY_CREDS_SECRET_NAME" \
+    --docker-server="$REGISTRY_URL" \
+    --docker-username="$username" \
+    --docker-password="$password" \
+    -n "$OPI_NAMESPACE"
+}
+
+create-registry-secret(){
+  scf_secrets="$(kubectl get secret "$SECRET_NAME" --namespace="$SCF_NAMESPACE" --export -o yaml | grep -E 'cc-.*|internal-ca-.*')"
+
+  cat <<EOT >> secret.yml
 ---
 apiVersion: v1
 kind: Secret
@@ -16,6 +33,9 @@ data:
 ${scf_secrets}
 EOT
 
-cat secret.yml
+  cat secret.yml
 
-kubectl apply -f secret.yml --namespace "$OPI_NAMESPACE"
+  kubectl apply -f secret.yml --namespace "$OPI_NAMESPACE"
+}
+
+main
