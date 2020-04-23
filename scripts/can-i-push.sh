687eeee1cef5c0898d49b6fa1ec0cfa2eafe2c61
Mario Nitchev <marionitchev@gmail.com>
2020-04-23 15:44:24 +0000

    Check eirini-release pipeline in can-i-push script
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- scripts/can-i-push.sh
+++ scripts/can-i-push.sh
@@ -2,33 +2,43 @@
 
 set -euo pipefail
 
-GREEN='\033[0;92m'
+RED='\033[38;2;0;0;0;48;2;255;0;0;1m'
 NORMAL='\033[0m'
 
-main(){
-  while read -r _ _ remote_ref _
-  do
-  if [[ "$remote_ref" =~ .*/develop$ ]]; then
+main() {
+  while read -r _ _ remote_ref _; do
+    if [[ "$remote_ref" =~ .*/master$ ]]; then
+      echo -e "${RED}   Do not push to master. Never.   ${NORMAL}"
+    fi
+    if [[ "$remote_ref" =~ .*/develop$ ]]; then
       check-ci
-  fi
+    fi
   done
 }
 
-check-ci(){
-  if curl -s https://ci.eirini.cf-app.com/api/v1/teams/main/pipelines/ci/jobs | grep -Eq "failed|error"; then
-    echo -e "Funny message: pipeline is ${GREEN}red.${NORMAL}"
-    echo "Please fix it before pushing"
+check-ci() {
+  if check-pipeline "ci" || check-pipeline "eirini-release"; then
+    echo -e "${RED}   Pipeline is red.   ${NORMAL}"
     prompt-push
   fi
   echo "CI is green"
 }
 
-prompt-push(){
-  read -r -p "Do you want to push in eirini-release? [y/N]" yn < /dev/tty
+prompt-push() {
+  read -r -p "Do you want to push in eirini-release? [y/N]" yn </dev/tty
   case $yn in
-      [Yy] ) exit 0 ;;
-      * ) echo 'Bailing out'; exit 1 ;;
+    [Yy]) exit 0 ;;
+    *)
+      echo 'Bailing out'
+      exit 1
+      ;;
   esac
+}
+
+check-pipeline() {
+  local pipeline_name failed_jobs
+  pipeline_name="$1"
+  curl -s "https://jetson.eirini.cf-app.com/api/v1/teams/main/pipelines/$pipeline_name/jobs" | grep -Eq "failed|error"
 }
 
 main
