308b4767d54df525265efee8c46c930cc2630bb9
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-08-07 15:03:40 +0000

    Add env injector config properties and yaml files
    Signed-off-by: Dimitris Karakasilis <dkarakasilis@suse.com>
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>
    Co-authored-by: Dimitris Karakasilis <dkarakasilis@suse.com>

--- deploy/scripts/cleanup.sh
+++ deploy/scripts/cleanup.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 PROJECT_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 
+kubectl delete mutatingwebhookconfigurations eirini-x-mutating-hook
 cat "$PROJECT_ROOT"/deploy/**/*.yml | kubectl delete -f -
