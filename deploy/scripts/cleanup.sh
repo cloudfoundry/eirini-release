5457cbe52b08ebfb238ec19c1d544a02226e3c2b
Mario Nitchev <marionitchev@gmail.com>
2020-09-14 14:10:04 +0000

    Remove unnecessary deletion of mutatingwebhookconfiguration
    This is already handled by declaring the empty
    mutatingwebhookconfiguration in the deployment yaml
    
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

--- deploy/scripts/cleanup.sh
+++ deploy/scripts/cleanup.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 PROJECT_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 
-kubectl delete mutatingwebhookconfigurations eirini-x-mutating-hook
 kubectl delete --recursive=true -f "$PROJECT_ROOT"/deploy
