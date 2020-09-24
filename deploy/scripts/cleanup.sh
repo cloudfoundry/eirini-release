619c0b955da7f2010932a0f73ffa949ae788b274
Kieron Browne <kbrowne@pivotal.io>
2020-09-24 17:20:55 +0100

    Wait for namespace deletion
    Ensures deploy can be scripted afterwards and will deploy on a clean
    cluster
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- deploy/scripts/cleanup.sh
+++ deploy/scripts/cleanup.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 PROJECT_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 
-kubectl delete --recursive=true -f "$PROJECT_ROOT"/deploy
+for ns in $(ls -1 $PROJECT_ROOT/deploy/**/namespace.yml); do
+  kubectl delete --wait=true -f "$ns" || true
+done
+
+# clean up remaining cluster-wide resources, if any
+kubectl delete --recursive=true -f "$PROJECT_ROOT"/deploy || true
