8d0e611c444ca570e0d87e70a6354fac6495ad7d
Danail Branekov <danailster@gmail.com>
2020-12-29 18:54:06 +0200

    Enable TLS for cloud controller
    We are thus testing TLS-enabled communication with cloud controller
    (which is mocked via wiremock on our test environments)
    
    The wiremock deployment itself needs additional parametters to enable
    HTTPS: port, keystore, keystore password, etc.
    
    [#175778846]

--- deploy/scripts/generate_eirini_tls.sh
+++ deploy/scripts/generate_eirini_tls.sh
@@ -8,6 +8,7 @@ mkdir -p keys
 trap 'rm -rf keys' EXIT
 
 otherDNS=$1
+keystore_password=$2
 
 pushd keys
 {
@@ -20,6 +21,21 @@ pushd keys
 
   if ! kubectl -n eirini-core get secret loggregator-certs >/dev/null 2>&1; then
     kubectl create secret -n eirini-core generic loggregator-certs --from-file=tls.crt=./tls.crt --from-file=ca.crt=./tls.crt --from-file=tls.key=./tls.key
+  fi
+
+  if ! kubectl -n eirini-core get secret capi-tls >/dev/null 2>&1; then
+    kubectl create secret -n eirini-core generic capi-tls --from-file=tls.crt=./tls.crt --from-file=ca.crt=./tls.crt --from-file=tls.key=./tls.key
+  fi
+
+  if ! kubectl -n eirini-core get secret wiremock-keystore >/dev/null 2>&1; then
+    pem_file=$(mktemp)
+    keystore_file=$(mktemp)
+    cat ./tls.key >"$pem_file"
+    cat ./tls.crt >>"$pem_file"
+    openssl pkcs12 -export -in "$pem_file" -out "$keystore_file" -password "pass:$keystore_password"
+    kubectl create secret -n eirini-core generic wiremock-keystore --from-file=keystore.pkcs12="$keystore_file" --from-literal=ks.pass="$keystore_password"
+    rm "$pem_file"
+    rm "$keystore_file"
   fi
 
   echo "Done!"
