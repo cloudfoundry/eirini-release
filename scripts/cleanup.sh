94caeda9a59de202be7e0b78fe19de890affac5b
Giuseppe Capizzi <gcapizzi@pivotal.io>
2021-02-16 15:24:59 +0000

    Also uninstall prometheus when cleaning up
    [#176846583]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -10,6 +10,7 @@ source "$SCRIPT_DIR/helpers/print.sh"
 delete_eirini() {
   helm --namespace "$SYSTEM_NAMESPACE" delete eirini || true
   helm --namespace "$SYSTEM_NAMESPACE" delete nats || true
+  helm --namespace "$SYSTEM_NAMESPACE" delete prometheus || true
   kubectl delete -f "$SCRIPT_DIR/assets/wiremock.yml" || true
 }
 
